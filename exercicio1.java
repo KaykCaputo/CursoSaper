f6061d792764893b0da11c6757300ecf0f5f5dd7
Kayk Caputo <kayk.caputo@gmail.com>
2023-04-11 20:49:06 -0300

    Primeira Aula, Exercicios 1 e 2

---
+++ exercicio1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Exercicios {
+    public static void main(String args[]) {
+        // Exercicio 1
+        float metros = 1260f, minutos = 4f;
+        System.out.println("||Exercicio 1:||");
+        System.out.println(
+                "João correu 1260 metros em 4 minutos:\n a)Qual a velocidade média de Jõao em metros por segundo? \n   R:Jõao correu em uma velocidade media de: "
+                        + metros / (minutos * 60) + "m/s");
+        System.out.println(
+                " b)Qual a velocidade média de Jõao em kilometros por hora?\n   R:Jõao correu em uma velocidade media de: "
+                        + (metros / 1000) / (minutos / 60) + "Km/h\n");
+        // Exercicio 2
+        System.out.println("||Exercicio 2:|| \n+Sistema de soma+");
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.println(" Digite o primeiro número: ");
+            int number1 = input.nextInt();
+            System.out.println(" Digite o segundo número: ");
+            int number2 = input.nextInt();
+            System.out.println(" Soma = " + (number1 + number2) + "\n");
+            input.close();
+        } catch (Exception error) {
+            System.out.println("Ocorreu um erro \n Erro: " + error);
+
+        }
+
+    }
+}
