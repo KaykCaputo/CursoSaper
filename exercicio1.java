6b73d37c48ac7be1b4b59d9b570f4fa480a603b3
Kayk Caputo <kayk.caputo@gmail.com>
2023-04-11 21:48:19 -0300

    Exercicios 1 e 2 Aula 1

--- exercicio1.java
+++ exercicio1.java
@@ -11,29 +11,15 @@ class Exercicios {
         System.out.println(
                 " b)Qual a velocidade média de Jõao em kilometros por hora?\n   R:Jõao correu em uma velocidade media de: "
                         + (metros / 1000) / (minutos / 60) + "Km/h\n");
-        /*
-         * // Exercicio 2
-         * System.out.println("||Exercicio 2:|| \n+Sistema de soma+");
-         * try {
-         * Scanner input = new Scanner(System.in);
-         * System.out.println(" Digite o primeiro número: ");
-         * int number1 = input.nextInt();
-         * System.out.println(" Digite o segundo número: ");
-         * int number2 = input.nextInt();
-         * System.out.println(" Soma = " + (number1 + number2) + "\n");
-         * input.close();
-         * } catch (Exception error) {
-         * System.out.println("Ocorreu um erro \n Erro: " + error);
-         * 
-         * }
-         */
-        // Exercicio 3
-        System.out.println("||Exercicio 3:||");
+        // Exercicio 2
+        System.out.println("||Exercicio 2:||");
+        System.out.println(
+                "Uma empresa corta barras de aço em tamanhos iguais, construa um programa onde o usuario digita o tamanho da barra \ne o que sobra da barra depois de corta-la em pedaços de 5 metros até não ser mais possivel: ");
         try {
             Scanner input = new Scanner(System.in);
             System.out.println(" Digite tamanho da barra: ");
             double comprimento = input.nextDouble();
-            System.out.printf("A barra pode ser cortada  %.0f vezes  e sobram %.2fmetros \n", comprimento / 5,
+            System.out.printf("A barra pode ser cortada  %.0f vezes  e sobram %.2f metros \n", comprimento / 5,
                     comprimento % 5);
             input.close();
         } catch (Exception error) {
